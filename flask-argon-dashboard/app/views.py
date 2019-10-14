1e09c82605ecbc5e242a5b458bca13f6cc5903cb
Gabriele Orazi <gabriorazi07@gmail.com>
2019-10-15 01:18:43 +0300

    init of APIs, no logic inside

--- flask-argon-dashboard/app/views.py
+++ flask-argon-dashboard/app/views.py
@@ -1,10 +1,11 @@
 # -*- encoding: utf-8 -*-
-from flask               import render_template, request, url_for, redirect, send_from_directory
-from werkzeug.exceptions import HTTPException, NotFound, abort
+from flask                  import render_template, request, url_for, redirect, send_from_directory
+from werkzeug.exceptions    import HTTPException, NotFound, abort
+from flask_api              import status
 
 import os, logging 
 
-from app        import app, lm, db, bc
+from app        import app, db, bc
 
 @app.route('/sitemap.xml')
 def sitemap():
@@ -51,3 +52,75 @@ def index(path):
         
         return render_template('layouts/auth-default.html',
                                 content=render_template( 'pages/404.html' ) )
+
+
+#####################################################
+####################### API #########################
+#####################################################
+
+# expected fields:
+#   serial:     Xbee serial number
+#   publicKey:  ECC public key generated on the node
+#   signature:  ECC-DSA signature over 'serial,publicKey' 
+@app.route('/api/v1/node/register', methods = ['POST'])
+def registerNewNode():
+    content = request.json 
+    return {}, status.HTTP_204_NO_CONTENT
+    res = {
+        "status": 400,
+        "message": "Invalid key or duplicate serial number."
+    }
+    return res, status.HTTP_400_BAD_REQUEST
+    res = {
+        "status": 401,
+        "message": "Invalid signature."
+    }
+    return res, status.HTTP_401_UNAUTHORIZED
+
+
+
+
+# expected fields:
+#   serial:     Xbee serial number
+#   cnonce:     Nonce for the request
+#   signature:  ECC-DSA signature over 'serial,cnonce' 
+@app.route('/api/v1/node/hello', methods = ['POST'])
+def initNode():
+    content = request.json 
+    res = {
+        "status": 200,
+        "message": "Handshake successful."
+    }
+    return res, status.HTTP_200_OK
+    res = {
+        "status": 400,
+        "message": "Invalid nonce."
+    }
+    return res, status.HTTP_400_BAD_REQUEST
+    res = {
+        "status": 401,
+        "message": "Invalid serial or signature."
+    }
+    return res, status.HTTP_401_UNAUTHORIZED
+
+
+
+
+# expected fields:
+#   serial:     Xbee serial number
+#   measurement[n .type]:  Sensor type
+#   measurement[n .value]:  Measurement
+@app.route('/api/v1/measurement', methods = ['POST'])
+def insertMeasurement():
+    content = request.json 
+    return {}, status.HTTP_204_NO_CONTENT
+    res = {
+        "status": 400,
+        "message": "Invalid sensor type."
+    }
+    return res, status.HTTP_400_BAD_REQUEST
+    res = {
+        "status": 401,
+        "message": "Invalid serial or signature."
+    }
+    return res, status.HTTP_401_UNAUTHORIZED
